aa10196fbf92b5e513a8c70422e72bd7bb670c9d
RaghunathVenkatachalaiah <raghunath.v28@gmail.com>
2025-03-18 11:19:36 +0530

    1st commit

---
+++ git_sample_framwork/src/main/java/genericUtility/BaseClass.java
@@ -0,0 +1,11 @@
+package genericUtility;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HI BaseClass");
+
+	}
+
+}
---
+++ git_sample_framwork/src/test/java/steveRaghu/ContactTest.java
@@ -0,0 +1,12 @@
+package steveRaghu;
+
+public class ContactTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Testclass");
+
+	}
+
+}
