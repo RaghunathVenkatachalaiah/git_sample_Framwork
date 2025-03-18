5a520d98bdf9fee58199e74f98dcae5950311f3e
RaghunathVenkatachalaiah <raghunath.v28@gmail.com>
2025-03-18 12:29:47 +0530

    contactpage created

--- git_sample_framwork/src/main/java/genericUtility/HomePage.java
+++ git_sample_framwork/src/main/java/genericUtility/HomePage.java
@@ -5,6 +5,6 @@ package genericUtility;
 public class HomePage {
 	
 	public static void main(String[]args) {
-		System.out.println("Hi");
+		System.out.println("Hi home page");
 }
 }
---
+++ git_sample_framwork/src/main/java/genericUtility/contactpage.java
@@ -0,0 +1,10 @@
+package genericUtility;
+
+public class contactpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
