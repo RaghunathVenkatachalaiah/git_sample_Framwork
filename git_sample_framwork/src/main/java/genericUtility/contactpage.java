e3579c1203124e80dfe6cd4ef7eccff2459e4745
RaghunathVenkatachalaiah <raghunath.v28@gmail.com>
2025-03-18 13:05:18 +0530

    push to new branch

--- git_sample_framwork/src/main/java/genericUtility/contactpage.java
+++ git_sample_framwork/src/main/java/genericUtility/contactpage.java
@@ -5,6 +5,8 @@ public class contactpage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("added ele 3");
+		
+		System.out.println("push to new branch");
 	}
 
 }
